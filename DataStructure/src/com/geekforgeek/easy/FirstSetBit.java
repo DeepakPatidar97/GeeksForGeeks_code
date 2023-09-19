ab1647f7a788b522c7fb690a6dc3d065ddc78cf8
dell <dell@DESKTOP-5SNIFH5>
2023-09-19 21:08:48 +0530

    problem of 19 sep

---
+++ DataStructure/src/com/geekforgeek/easy/FirstSetBit.java
@@ -0,0 +1,30 @@
+package com.geekforgeek.easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class FirstSetBit {
+
+	public static void main(String[] args) {
+		System.out.println(getFirstSetBit(18));
+		System.out.println(getFirstSetBit(12));
+	}
+	public static int getFirstSetBit(int n){
+        LinkedList<Integer> bit = new LinkedList();
+        while(n!=0) {
+        	bit.add((n%2));
+        	n = n/2;
+        }
+        int count = 0;
+        for (Integer temp : bit) {
+        	count++;
+        	if(temp==1) {
+        		break;
+        	}
+        	
+		}
+        return count;  
+    }
+
+}
